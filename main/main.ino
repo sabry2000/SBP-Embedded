4a847d64142183bd57e2f7f7b9d84ae9cf0a2508
Ahmed Sabry <asabry@ineosolutionsinc.com>
2022-02-06 21:36:43 -0800

    Initial commit

---
+++ main/main.ino
@@ -0,0 +1,18 @@
+const int USB1_CTRL = 16;
+const int USB2_CTRL = 17;
+const int USB3_CTRL = 5;
+const int USBC_CTRL = 18;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(USB1_CTRL, OUTPUT);
+  pinMode(USB2_CTRL, OUTPUT);
+  pinMode(USB3_CTRL, OUTPUT);
+  pinMode(USBC_CTRL, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
